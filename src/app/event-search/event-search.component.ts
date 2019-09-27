32e2a7df8d5b608aab4b21c06373ce821ebb7493
Emad Messiha <emadmessiha@gmail.com>
2019-09-27 10:58:02 -0400

    added events search and display

--- src/app/event-search/event-search.component.ts
+++ src/app/event-search/event-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {MatSnackBar} from '@angular/material/snack-bar';
 import { EventsService } from '../services/events.service';
@@ -6,6 +6,8 @@ import { EventItem } from '../models/event.model';
 import { PagedEvents } from '../models/paged-events.model';
 import { HttpErrorResponse } from '@angular/common/http';
 import { BaseComponent } from '../base-component';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 
 @Component({
@@ -17,6 +19,7 @@ import { BaseComponent } from '../base-component';
   ]
 })
 export class EventSearchComponent extends BaseComponent implements OnInit {
+  displayedColumns: string[] = ['event_date', 'event_type', 'event_summary', 'event_size'];
   defaultStartDate = new Date(2015, 10, 18);
   defaultDuration = '30';
   startDateControl = new FormControl(this.defaultStartDate, [Validators.required]);
@@ -26,6 +29,11 @@ export class EventSearchComponent extends BaseComponent implements OnInit {
     duration: this.durationControl
   });
   events: EventItem[] = [];
+  currentPageIndex = 0;
+  currentPageSize = 20;
+  currentTotalResults = 0;
+  dataSource = new MatTableDataSource<EventItem>(this.events);
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   constructor(
     private snackBar: MatSnackBar,
@@ -34,12 +42,19 @@ export class EventSearchComponent extends BaseComponent implements OnInit {
     }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
   submitSearch() {
     if (this.searchForm.valid) {
-      this.eventsService.searchEvents(this.startDateControl.value, this.durationControl.value).subscribe((pagedEvents: PagedEvents) => {
-        console.log(pagedEvents);
+      this.eventsService.searchEvents(
+        this.startDateControl.value,
+        this.durationControl.value,
+        this.currentPageSize,
+        this.currentPageIndex)
+        .subscribe((pagedEvents: PagedEvents) => {
+          this.events = pagedEvents.data;
+          this.currentTotalResults = pagedEvents.totalResults;
       },
       (err: HttpErrorResponse) => {
         this.openMessageSnackBar('Server response: ' + err.status + ' - ' + err.statusText);
@@ -47,6 +62,16 @@ export class EventSearchComponent extends BaseComponent implements OnInit {
     } else {
       this.openMessageSnackBar('Search invalid, start date must be a valid date');
     }
+  }
+
+  pageChange(pagingEvent: PageEvent) {
+    this.currentPageIndex = pagingEvent.pageIndex;
+    this.currentPageSize = pagingEvent.pageSize;
+    this.submitSearch();
+  }
+
+  onRowClicked(row) {
+    console.log(row);
   }
 
 }
